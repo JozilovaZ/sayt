4cba17b38f1aabfe14bfee74cdaede4112974143
JozilovaZ <jozilovazuxra8899@gmail.com>
2025-04-20 18:57:14 +0500

    redis settings

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': 'redis://redis:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
