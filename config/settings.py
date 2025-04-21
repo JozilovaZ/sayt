41f2c867af7c9071068d37bfcbb798fcd1c019a4
JozilovaZ <jozilovazuxra8899@gmail.com>
2025-04-21 15:59:55 +0500

    redis sozlamalari

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,8 @@ CACHES = {
 # CELERY_BROKER_URL = 'redis://redis:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
-CELERY_BROKER_URL = 'redis://master_redis_host:6379/0'
-CELERY_RESULT_BACKEND = 'redis://master_redis_host:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
